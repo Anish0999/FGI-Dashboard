77224e50ef6ecb7f83181b8c5d304fc9be88b5c9
Anish0999 <anishgupta0999@gmail.com>
2024-10-02 23:26:31 -0400

    Connected MongoDB with backend

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,25 @@
+require('dotenv').config();
 const express = require('express');
-// test2222
+const mongoose = require('mongoose');
+
+const app = new express();
+
+
+
+
+
+
+
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Listening on port ${process.env.PORT}`);
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+
+
